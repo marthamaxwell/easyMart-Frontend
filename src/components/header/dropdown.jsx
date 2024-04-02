3ce21d4dc4fc800c174cff28c0a43e3aedd29aee
marthamaxwell <marthamaxwell499@gmail.com>
2024-04-02 20:33:27 +0100

    header

--- src/components/header/dropdown.jsx
+++ src/components/header/dropdown.jsx
@@ -1,34 +1,69 @@
 import React from "react";
-import Header from "./header";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXmark,
+  faCartShopping,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
 
-const DropDown = () => {
-  const dropdownArray = [
-    {
-      id: 1,
-      name: "Mobile Phones",
-    },
-
-    {
-      id: 2,
-      name: "Top Brand Phones",
-    },
-
-    {
-      id: 3,
-      name: "Mobile Phones Accessories",
-    },
-    {
-      id: 4,
-      name: "Top smartphones",
-    },
-  ];
+const Navbars = ({ OpenMenu, Open }) => {
   return (
-    <div>
-      {dropdownArray.map((options) => (
-        <Header key={options.name} property={options} />
-      ))}
+    <div
+      className={
+        Open
+          ? "max-sm:flex flex-col hidden items-center absolute h-full justify-center inset-0 left-1/4  bg-pri"
+          : "hidden"
+      }
+    >
+      <nav className="">
+        <FontAwesomeIcon icon={faXmark} onClick={OpenMenu} />
+        <Link to={""} className="">
+          <div className="">
+            <FontAwesomeIcon
+              icon={faUser}
+              className="h-7 absolute -ml-9 mt-2 text-nav"
+            />
+
+            <ul>
+              <li className="text-log">Login</li>
+              <li className="text-nav">Account</li>
+            </ul>
+          </div>
+        </Link>
+        <Link to={""}>
+          <div className="">
+            {" "}
+            <FontAwesomeIcon
+              className="h-7 absolute -ml-9 mt-2 text-nav"
+              icon={faHeart}
+            />
+            <ul>
+              <li className="text-nav">Favourite</li>
+              <li className="text-nav">List</li>
+            </ul>
+          </div>
+        </Link>
+        <Link to={""}>
+          <div className=" flex ">
+            <FontAwesomeIcon
+              className="h-7 absolute -ml-10 mt-2 text-nav"
+              icon={faCartShopping}
+            />
+
+            <ul>
+              <li className="text-nav">Total</li>
+              <li className="text-nav">0NGN</li>
+            </ul>
+            <div className="w-5 h-5 flex justify-center bg-red-700 text-white text-sm rounded-xl -ml-16 z-10 ">
+              0
+            </div>
+          </div>
+        </Link>
+      </nav>
     </div>
   );
 };
 
-export default DropDown;
+export default Navbars;
