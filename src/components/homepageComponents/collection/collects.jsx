1f08afa78b3da57d0e75907bd5ed4e57207f447f
marthamaxwell <marthamaxwell499@gmail.com>
2024-04-05 06:46:48 +0100

    header

--- src/components/homepageComponents/collection/collects.jsx
+++ src/components/homepageComponents/collection/collects.jsx
@@ -6,26 +6,18 @@ const COllection = () => {
     {
       id: 1,
       image: TvImage,
-      name: "BILL PAYMENT",
-      title: "Settle Electricity, Tv and Internet",
     },
     {
       id: 2,
       image: TransImage,
-      name: "TRANSACTION",
-      title: "Send and Receive Money",
     },
     {
       id: 3,
       image: PcImage,
-      name: "SETTLE INVOICE",
-      title: "Settle payments with ease",
     },
     {
       id: 4,
       image: DrugImage,
-      name: "PHARMACY AND DRUGS",
-      title: "Medical and Health care",
     },
   ];
   return (
